4fefa866512d07064a75a1a19f12fb821e94928c
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2023-09-28 11:43:31 -0300

    chore: add first spring configurations

---
+++ src/main/java/com/factfusion/fusionapp/FusionappApplication.java
@@ -0,0 +1,13 @@
+package com.factfusion.fusionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionappApplication.class, args);
+	}
+
+}
