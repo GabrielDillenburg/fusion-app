3ef9e928f11e9a9c779ecdbdf4a34f48bdf9bfc7
Gabriel Dillenburg <47983867+GabrielDillenburg@users.noreply.github.com>
2023-11-24 12:10:27 -0300

    Update OpenAiServiceConfig.java

--- src/main/java/com/factfusion/fusionapp/config/OpenAiServiceConfig.java
+++ src/main/java/com/factfusion/fusionapp/config/OpenAiServiceConfig.java
@@ -8,7 +8,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class OpenAiServiceConfig {
     @Bean
     public OpenAiService openAiService() {
-        return new OpenAiService("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"); // You may need to configure this service if it requires parameters.
+        return new OpenAiService("some-fake-token"); // You may need to configure this service if it requires parameters.
     }
 }
 
